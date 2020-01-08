78d5dfcf40e63aff6926d029ce49a58b0a2fae3c
Chandhana Surabhi Hariharan <36557783+SurabhiHariharan@users.noreply.github.com>
2020-01-08 16:51:27 +0530

    Add files via upload
    Check equal number of x and o in input

---
+++ xando.go
@@ -0,0 +1,28 @@
+package main
+
+
+import(
+	"fmt"
+	"strings"
+)
+
+
+func main(){
+	var array string
+	fmt.Scan(&array)
+	status := find(array)
+	fmt.Println("There is a match in 0's and X's",status)
+}
+
+func find(array string)bool{
+	countx:=0
+	counto:=0
+		
+	countx=strings.Count(strings.ToLower(array),"x")
+	counto=strings.Count(strings.ToLower(array),"o")
+		
+	if counto == countx {
+		return true
+	}
+	return false
+}
